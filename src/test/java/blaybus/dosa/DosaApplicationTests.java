97c6c0d2ae01040a7b68145c07408ca2b400d3b3
3eungyeon <3eungyeon@naver.com>
2026-02-05 01:00:03 +0900

    init: spring boot project structure

---
+++ src/test/java/blaybus/dosa/DosaApplicationTests.java
@@ -0,0 +1,13 @@
+package blaybus.dosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DosaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
